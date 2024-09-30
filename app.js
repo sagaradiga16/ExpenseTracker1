04f2c10712bd50a4c78fc7fd15379123c77aa3ea
Sagar1617 <sagar.adiga@gmail.com>
2024-09-30 23:31:53 +0530

    ExpenseTracker

---
+++ app.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const cors = require("cors");
+const connectDB = require("./db/db");
+const {readdirSync} = require('fs');
+const { route } = require("./routes/transactions");
+const app = express()
+
+require("dotenv").config()
+
+const PORT = process.env.PORT
+
+//middlewares
+app.use(express.json())
+app.use(cors())
+
+//routes
+readdirSync('./routes').map((route) =>{
+    app.use('/api/v1', require('./routes/' + route))
+})
+
+const server = () =>{
+    connectDB()
+    app.listen(PORT, () => {
+        console.log("Listening to port:", PORT);
+        
+    })
+    
+}
+server()
+
